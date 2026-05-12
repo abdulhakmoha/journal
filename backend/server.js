0e9319d636e78b9396afa83bc0dc8c6339439565
abdi alu <abdulhakmoha@example.com>
2026-05-11 21:52:57 -0700

    fix: resolve syntax error in server.js to allow deployment

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
   next();
+});
 
 app.use('/uploads', express.static('uploads'));
 
